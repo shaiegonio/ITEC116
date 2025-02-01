07289cd2aba89e24405287961b6f7deb737dcb40
SHAIMAH <137877379+shaiegonio@users.noreply.github.com>
2025-02-01 21:30:47 +0800

    Delete ITEC116 directory

--- ITEC116/Lab4/find_api.py
+++
@@ -1,9 +0,0 @@
-import os
-
-for root, dirs, files in os.walk("."):
-    for file in files:
-        if file.endswith(".py"):
-            with open(os.path.join(root, file), "r", errors="ignore") as f:
-                for line in f:
-                    if "API_KEY" in line:
-                        print(f"Found in {file}: {line.strip()}")
